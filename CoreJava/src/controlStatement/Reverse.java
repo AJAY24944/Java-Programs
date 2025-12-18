64f828da68eec62c6f74b9eedb4273ba581de968
AJAY24944 <ellandulajaynani@gmail.com>
2025-12-18 17:56:47 +0530

    Reverse code

--- CoreJava/src/controlStatement/Reverse.java
+++ CoreJava/src/controlStatement/Reverse.java
@@ -3,20 +3,22 @@ package controlStatement;
 public class Reverse {
 	
 	public boolean isReverse(int num) {
-		int temp = num;
 		int rev = 0;
 		while(num != 0) {
 			int rem = num%10;
 			rev = rev*10+rem;
 			num/=10;
 		}
-		return temp == rev;
+		System.out.println("Reversed Number : " + rev);
+		return true;
 		
 	}
 
 	public static void main(String[] args) {
+		
+		
 		Reverse obj = new Reverse();
-		System.out.println(obj.isReverse(123)?"Palindrome" : "Not Palindrome");
+		obj.isReverse(12345);
 		
 		
 	}
